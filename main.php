face9c101f377ffc1ea205a4d7dedc3e797b9823
Fujii Mika <67635291+aomikan10@users.noreply.github.com>
2022-01-19 11:32:26 +0900

    main
    カリカリカリカリ

---
+++ main.php
@@ -0,0 +1,107 @@
+<?php $__env->startSection('title','知るカフェ会員とは'); ?>
+
+<?php echo $__env->make('layouts.head', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->make('layouts.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<?php $__env->startSection('content'); ?>
+<div class="l-wrapper">
+    <h1 class="title-section-h1">
+        <div class="title-section-h1-en"><img src="<?php echo e(asset('images/pages/user/about/title.svg')); ?>" alt="About"></div>
+        <div class="title-section-h1-ja">知るカフェ会員とは</div>
+    </h1>
+
+    <div class="l-section mb-30">
+        <div class="l-inner">
+            <p>
+                <img src="<?php echo e(asset('images/pages/user/member/00_pc_tokuten_mv.jpg')); ?>" alt="SHIRUCAFE 会員特典" class="pc">
+                <img src="<?php echo e(asset('images/pages/user/member/00_sp_tokuten_mv.jpg')); ?>" alt="SHIRUCAFE 会員特典" class="sp">
+            </p>
+        </div>
+    </div>
+
+    <h2 class="title-section-h2"><span class="title-section-h2-content">知るカフェ会員特典</span></h2>
+    <div class="l-section mb-30">
+        <div class="l-inner">
+
+            <div class="box text-center">
+                <p class="text-size-m mb-0">知るカフェアプリのダウンロードはこちらから</p>
+                <div class="row no-gutters wid-320 wid-md-100per" style="margin:auto;">
+                    <div class="col-6 col-md-12">
+                        <a href="https://itunes.apple.com/jp/app/shiru-cafe/id1440790908?mt=8" style="display:inline-block;overflow:hidden;background:url(https://linkmaker.itunes.apple.com/en-us/badge-lrg.svg?releaseDate=2018-11-04&amp;kind=iossoftware&amp;bubble=apple_music) no-repeat;width:135px;height:40px;margin-top:10px;" target="_blank" rel="noopener noreferrer"></a>
+                    </div>
+                    <div class="col-6 col-md-12">
+                        <a href="https://play.google.com/store/apps/details?id=jp.co.enrission.shirucafe&amp;pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1" target="_blank" rel="noopener noreferrer"><img style="width:155px;height:60px;" alt="Get it on Google Play" src="https://play.google.com/intl/us-en/badges/images/generic/en_badge_web_generic.png"></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-30">
+
+                <div class="col-6 col-md-12 mb-20">
+                    <div class="box pt-0 pl-0 pr-0">
+                        <div class="box-list-title">1.ドリンクが無料で飲める</div>
+                        <div class="box-list-content">
+                            <div class="box-list-image"><img src="<?php echo e(asset('images/pages/user/member/01.png')); ?>" alt="1.ドリンクが無料で飲める"></div>
+                            <p class="box-list-text mt-20">淹れたてのコーヒー・すっきりとした味わいの紅茶・甘酸っぱいオレンジジュースなど、約10種類のこだわりのドリンクを無料でお召し上がりいただけます。90分ごとにおかわりも無料でできます</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-6 col-md-12 mb-20">
+                    <div class="box pt-0 pl-0 pr-0">
+                        <div class="box-list-title">2.Wi-Fi・コンセント使いたい放題</div>
+                        <div class="box-list-content">
+                            <div class="box-list-image"><img src="<?php echo e(asset('images/pages/user/member/02.png')); ?>" alt="2.Wi-Fi・コンセント使いたい放題"></div>
+                            <p class="box-list-text mt-20">課題やゼミ活動など大学生に欠かせないようにサクサクと繋がるWi-Fiとコンセントも完備しております。<br class="pcbr">&nbsp;</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-6 col-md-12 mb-20">
+                    <div class="box pt-0 pl-0 pr-0">
+                        <div class="box-list-title">3.カフェで社会人と気軽に交流できる</div>
+                        <div class="box-list-content">
+                            <div class="box-list-image"><img src="<?php echo e(asset('images/pages/user/member/03.png')); ?>" alt="3.カフェで社会人と気軽に交流できる"></div>
+                            <p class="box-list-text mt-20">カフェの空間で私服で気軽に企業の方と交流することができます。業界や気になったことを聞く事で将来についての漠然な不安を解消する事ができます。</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-6 col-md-12 mb-20">
+                    <div class="box pt-0 pl-0 pr-0">
+                        <div class="box-list-title">4.人気ケーキを低価格で楽しめる</div>
+                        <div class="box-list-content">
+                            <div class="box-list-image"><img src="<?php echo e(asset('images/pages/user/member/04.png')); ?>" alt="4.人気ケーキを低価格で楽しめる"></div>
+                            <p class="box-list-text mt-20">毎日にちょっとしたご褒美を。<br>ケーキ販売を低価格で行っております。学生のお財布にとても優しく、毎月入れ替わるので季節の商品を楽しむことができます。</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-6 col-md-12 mb-20">
+                    <div class="box pt-0 pl-0 pr-0">
+                        <div class="box-list-title">5.団体でも利用可能</div>
+                        <div class="box-list-content">
+                            <div class="box-list-image"><img src="<?php echo e(asset('images/pages/user/member/06.png')); ?>" alt="5.団体でも利用可能"></div>
+                            <p class="box-list-text mt-20">ゼミやサークルの会議などミーティングの場としても利用する事ができます。</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-6 col-md-12 mb-20">
+                    <div class="box pt-0 pl-0 pr-0">
+                        <div class="box-list-title">6.SHIRU WORKに参加できる</div>
+                        <div class="box-list-content">
+                            <div class="box-list-image"><img src="<?php echo e(asset('images/pages/user/member/07.png')); ?>" alt="6.SHIRU WORKに参加できる"></div>
+                            <p class="box-list-text mt-20">ワークショップを通して実際の仕事の一部を体験できます。無料で参加できるので1・2年生には特に人気のイベントになります。</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<?php echo $__env->make('layouts.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/src/resources/views/user/member.blade.php ENDPATH**/ ?>
